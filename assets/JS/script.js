beba2a58288c395233725e646281d8e9b979a71c
Lucas Zapico <zapicolucas@gmail.com>
2018-02-06 18:26:06 -0800

    value section added

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+// const ABOUT = document.querySelector('#about');
+// const PROJECT = document.querySelector('#projects');
+// const WORKSHOP = document.querySelector('#workshop');
+// const VALUES = document.querySelector('#values');
+//
+// ABOUT.addEventListener('click', function(){
+//   window.scroll({
+//     left: 0,
+//     top: 500,
+//     behavior: 'smooth'
+//   );
+// });
